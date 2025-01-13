03f5ad8a697967a462c6561b9e2b6081a973652e
YHGLeader <doorgadgettes@gmail.com>
2025-01-13 10:15:06 +0200

    Update 88br-Derbi-Adventure.py

--- Source/88br-Derbi-Adventure.py
+++ Source/88br-Derbi-Adventure.py
@@ -1,1 +1,19 @@
-
+import random
+input("jos rakennat ittelles 88br derbin ja kohdistat keulan suoraan keulakulmille peli - paina enter aloittaaksesi")
+inval = input("sinulta puuttuu osia! a: improvisoi kotoa osia. b: ostat osat kaupasta c:  korjaat purukumilla")
+inval2 = input("a: 30pwk vai b: 21phbg")
+inval3 = input("a: voca 50/70 b: voca 80/90")
+if inval == a: dql = 0
+if inval == a: mon = 10
+if inval == b: dql = 1
+if inval == b: mon = 3
+if inval == c: dql = -1
+if inval == c: mon = 10
+if inval == else: 
+if inval2 == a: d1 = 1
+if inval2 == b: d1 = -1
+if inval3 == a: d2 = -1
+if inval3 == b: d1 = 1
+ranval = (random.randint(0.12))
+ranval = int(ranval)
+derb = ranval + dql + d1 + d2
