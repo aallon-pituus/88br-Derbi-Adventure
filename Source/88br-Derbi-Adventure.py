551268e2a5ae6c746d481b307e9eb2b900b15b54
aallon-pituus <119002731+aallon-pituus@users.noreply.github.com>
2025-01-21 17:56:21 +0200

    Update 88br-Derbi-Adventure.py
    Updated game

--- Source/88br-Derbi-Adventure.py
+++ Source/88br-Derbi-Adventure.py
@@ -1,14 +1,26 @@
 # Tuo vaadittu skripti
-from gameResources import gamefunctions as game
+from gameResources import gamefunctions as game # VS Codelle: #type: ignore
 
 # Avaa päävalikko
 game.start()
 
 # Anna muuttujille oletusarvot
 game.setup()
+choice_downstairs = ""
 
-# Valitse osat
-game.derbi_osat()
+# Heräät talossa
+choice_bedroom = game.place_bedroom()
+if choice_bedroom == "a":
+    # Menet alakertaan
+    choice_downstairs = game.place_downstairs()
+    if choice_downstairs == "c":
+        # Valitse osat
+        game.derbi_osat()
 
-# Viritä derbi
-game.derbi_viritys()
+        # Viritä derbi
+        game.derbi_viritys()
+
+        # Pärinät
+        game.derbi_ääni()
+elif choice_bedroom == "b":
+    game.place_desk()
