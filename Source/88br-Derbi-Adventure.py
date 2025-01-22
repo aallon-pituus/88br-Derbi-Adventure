44667af7fed2fa3bdf2ff5c4226405cb986f8d0f
aallon-pituus <119002731+aallon-pituus@users.noreply.github.com>
2025-01-22 16:34:29 +0200

    Update 88br-Derbi-Adventure.py
    New update

--- Source/88br-Derbi-Adventure.py
+++ Source/88br-Derbi-Adventure.py
@@ -1,26 +1,38 @@
 # Tuo vaadittu skripti
 from gameResources import gamefunctions as game
+from gameResources import string_table as tab
 
 # Avaa päävalikko
 game.start()
 
 # Anna muuttujille oletusarvot
 game.setup()
-choice_downstairs = ""
 
 # Heräät talossa
-choice_bedroom = game.place_bedroom()
-if choice_bedroom == "a":
-    # Menet alakertaan
-    choice_downstairs = game.place_downstairs()
-    if choice_downstairs == "c":
-        # Valitse osat
-        game.derbi_osat()
-
-        # Viritä derbi
-        game.derbi_viritys()
-
-        # Pärinät
-        game.derbi_ääni()
-elif choice_bedroom == "b":
-    game.place_desk()
+while True: # Makuuhuone loop
+    choice_bedroom = game.place_bedroom()
+    if choice_bedroom == "a": # Jos menet alakertaan
+        choice_downstairs = game.place_downstairs()
+        if choice_downstairs == "c": # Jos menet takapihalle
+            game.derbi_osat() # Valitse osat
+            game.derbi_viritys() # Viritä derbi
+            game.derbi_ääni() # Pärinät
+    if choice_bedroom == "b": # Jos menet pöydän äärelle
+        while True: # Pöytä loop
+            choice_desk = game.place_desk()
+            if choice_desk == "a": # Jos valitset A pöydän luona
+                while True: # Tietokone loop
+                    choice_computer = game.item_computer()
+                    if choice_computer == "a": # Jos avaat Chromen
+                        while True: # Chrome loop
+                            choice_chrome = game.app_chrome()
+                            if choice_chrome == "b": # Jos avaat incognito-tilan
+                                game.clear_screen()
+                                print(tab.lose())
+                                game.slow_print("Hävisit pelin. Sinut huomattiin menemässä incognito-tilaan.")
+                                game.stats_print()
+                                exit()
+                            if choice_chrome == "c": # Jos sammutat tietokoneen
+                                break # Mene takaisin pöydän äärelle
+            if choice_desk == "b": # Jos valitset B pöydän luona
+                break # Mene takaisin makuuhuoneeseen
